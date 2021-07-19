b80c9d42343d323a542196eab04aac9674b08819
Tom Clouse <tommyclouse@yahoo.com>
2021-07-19 14:26:11 -0500

    corrected typo

--- locations/request.py
+++ locations/request.py
@@ -60,7 +60,7 @@ def get_all_locations():
 #     return requested_location
 
 def get_single_location(id):
-    with sqlite3.connect("/kennel.db") as conn:
+    with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
